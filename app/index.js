396b3c34f3400c85ad9b7d6a95acb6c6476eaebf
dirtydeM0n <31212069+dirtydeM0n@users.noreply.github.com>
2019-01-06 16:39:17 +0500

    Chain replacement and syncing
    The updated chain is synced and replaced with all the socket peers connected to the first instance.

--- app/index.js
+++ app/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser'); // acts as a middleware that converts requests ands allows json to be received within post requests
 
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001; // so that any custom port can also be used
+
 const app = express(); // creates an express application with all the functionality of express framework
 app.use(bodyParser.json());
 
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc); // the above blockchain instance is passed
 
 // request and response are the parameters of the arrow function
 // '/' is the endpoint that interacts with the api of the instance created
@@ -16,11 +19,15 @@ app.get('/blocks', (req, res) => {       // returns the blocks of the current bl
 });
 // when users make a post request, express will automatically create an object called data
 // body object contains other objects and data that were send in the json post request
+
 app.post('/mine', (req, res) => { 
 const block = bc.addBlock(req.body.data); // we assume that data field is present in the mine object
 console.log(`New block added: ${block.toString()}`);
 // redirect is an express built-in function of the res object
 res.redirect('/blocks'); // redirects to the get request of blocks endpoint
+
+p2pServer.syncChains(); //synching the blockchain function
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`)); // es6 template string
+p2pServer.listen(); // this will start the websocket server in this blockchain application instance
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -16,10 +16,39 @@ class P2pServer {
     listen() {
         const server = new Websocket.Server({ port: P2P_PORT }); // static class Server present in websocket modules
         server.on('connection', socket => this.connectSocket(socket)); // event listener which listens for incoming messages sent to the websocket server
+        this.connecToPeers();
         console.log(`Listening for P2P connections on the port: ${ P2P_PORT}`);
     }
+    connecToPeers() {
+        peers.forEach( peer => {
+            // a peer will look like ws://localhost:5001
+            const socket = new Websocket(peer); // each socket will have the peer
+            socket.on('open', () => this.connectSocket(socket)); // event listener for each socket connection of each peer
+        });
+    }
+    // all sockets run through the connectsockets function below
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket Connected');
+        this.messageHandler(socket);
+        this.sendChain(socket);
+
+    }
+    messageHandler(socket){
+        socket.on('message', message => {
+            const data = JSON.parse(message); // converts the stringified message into a JSobject
+            // console.log('data', data);
+            this.blockchain.replaceChain(data);
+    
+        });
+    }
+    // helper function to avoid duplication
+    sendChain(socket) {
+        socket.send(JSON.stringify(this.blockchain.chain)); // sending message to each socket
+    }
+    // synchain will send updated blockahin of this current instance to all of the socket peers
+    syncChains() {
+        this.sockets.forEach(socket => this.sendChain(socket));
     }
 }
+module.exports = P2pServer;
